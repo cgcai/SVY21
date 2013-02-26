13127ee4e9ef7317f92c55a6c7b6f133b2754036
Camillus Gerard Cai <cgcai@qxcg.net>
2013-02-26 21:40:13 +0800

    Javadoc and shorthand methods for conversions with java.util.Map.

--- Java/SVY21.java
+++ Java/SVY21.java
@@ -1,9 +1,17 @@
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * 
+ * @author cgcai
+ * 
+ * The SVY21 class provides functionality to convert between the SVY21 and Lat/Lon coordinate systems.
+ * 
+ * Internally, the class uses the equations specified in the following web page to perform the conversion.
+ * http://www.linz.govt.nz/geodetic/conversion-coordinates/projection-conversions/transverse-mercator-preliminary-computations/index.aspx
+ */
 public class SVY21 {
-	// Ref: http://www.linz.govt.nz/geodetic/conversion-coordinates/projection-conversions/transverse-mercator-preliminary-computations/index.aspx
-
+	
 	// Map Keys
 	private static final String KEY_EASTING = "easting";
 	private static final String KEY_NORTHING = "northing";
@@ -12,20 +20,61 @@ public class SVY21 {
 
 	private final static double radRatio = Math.PI / 180;
 
+	/**
+	 * Returns a String representing a Key in a Java Map associated with an SVY21 Northing.
+	 * 
+	 * @return the key associated with an SVY21 Easting in a result.
+	 */
+	public static String eastingKey() {
+		return KEY_EASTING;
+	}
+	/**
+	 * Returns a String representing a Key in a Java Map associated with Latitude.
+	 * 
+	 * @return the key associated with Latitude in a result.
+	 */
+	public static String latitudeKey() {
+		return KEY_LATITUDE;
+	}
+	/**
+	 * Returns a String representing a Key in a Java Map associated with Longitude.
+	 * 
+	 * @return the key associated with Longitude in a result.
+	 */
+	public static String longitudeKey() {
+		return KEY_LONGITUDE;
+	}
+	/**
+	 * Returns a String representing a Key in a Java Map associated with an SVY21 Northing.
+	 * 
+	 * @return the key associated with an SVY21 Northing in a result.
+	 */
+	public static String northingKey() {
+		return KEY_NORTHING;
+	}
 	// Datum and Projection
 	final double a		= 6378137;
 	final double f		= 1 / 298.257223563;
 	final double oLat	= 1.366666;				// degrees
+
 	final double oLon	= 103.833333;			// degrees
-	final double No		= 38744.572;
+	final double No		= 38744.572;	
 	final double Eo		= 28001.642;
+
 	final double k		= 1;
 
 	// Projection Constants
 	final double b;
-	final double e2, e4, e6;	
+
+	final double e2, e4, e6;
+
 	final double A0, A2, A4, A6;
 
+	/**
+	 * Class constructor.
+	 * 
+	 * Computes various projection constants based on WGS84 and SVY21.
+	 */
 	public SVY21() {
 		b = a * (1 - f);
 
@@ -39,78 +88,74 @@ public class SVY21 {
 		A6 = 35 * e6 / 3072;
 	}
 
-	public Map<String, Double> computeSVY21(double lat, double lon) {
-		double latR = lat * radRatio;
-		double sinLat = Math.sin(latR);
-		double sin2Lat = sinLat * sinLat;
-		double cosLat = Math.cos(latR);
-		double cos2Lat = cosLat * cosLat;
-		double cos3Lat = cos2Lat * cosLat;
-		double cos4Lat = cos3Lat * cosLat;
-		double cos5Lat = cos3Lat * cos2Lat;
-		double cos6Lat = cos5Lat * cosLat;
-		double cos7Lat = cos5Lat * cos2Lat;
-
-		double rho = calcRho(sin2Lat);
-		double v = calcV(sin2Lat);
-		double psi = v / rho;
-		double t = Math.tan(latR);
-		double w = (lon - oLon) * radRatio;
-
-		double M = calcM(lat);
-		double Mo = calcM(oLat);
-
-		double w2 = w * w;
-		double w4 = w2 * w2;
-		double w6 = w4 * w2;
-		double w8 = w6 * w2;
-
-		double psi2 = psi * psi;
-		double psi3 = psi2 * psi;
-		double psi4 = psi2 * psi2;
-
-		double t2 = t * t;
-		double t4 = t2 * t2;
-		double t6 = t4 * t2;
-
-		// Compute Northing.
-		double nTerm1 = w2 / 2 * v * sinLat * cosLat;
-		double nTerm2 = w4 / 24 * v * sinLat * cos3Lat * (4 * psi2 + psi - t2);
-		double nTerm3 = w6 / 720 * v * sinLat * cos5Lat * ((8 * psi4) * (11 - 24 * t2) - (28 * psi3) * (1 - 6 * t2) + psi2 * (1 - 32 * t2) - psi * 2 * t2 + t4);
-		double nTerm4 = w8 / 40320 * v * sinLat * cos7Lat * (1385 - 3111 * t2 + 543 * t4 - t6);
-		double N = No + k * (M - Mo + nTerm1 + nTerm2 + nTerm3 + nTerm4);
-
-		// Compute Easting.
-		double eTerm1 = w2 / 6 * cos2Lat * (psi - t2);
-		double eTerm2 = w4 / 120 * cos4Lat * ((4 * psi3) * (1 - 6 * t2) + psi2 * (1 + 8 * t2) - psi * 2 * t2 + t4);
-		double eTerm3 = w6 / 5040 * cos6Lat * (61 - 479 * t2 + 179 * t4 - t6);
-		double E = Eo + k * v * w * cosLat * (1 + eTerm1 + eTerm2 + eTerm3);
-
-		Map<String, Double> result = new TreeMap<String, Double>();
-		result.put(northingKey(), N);
-		result.put(eastingKey(), E);
-		return result;
-	}
-
 	private double calcM(double lat) {
 		double latR = lat * radRatio;
 		double m = a * ((A0 * latR) - (A2 * Math.sin(2 * latR)) + (A4 * Math.sin(4 * latR)) - (A6 * Math.sin(6 * latR)));
 		return m;
 	}
-
+	
 	private double calcRho(double sin2Lat) {
 		double num = a * (1 - e2);
 		double denom = Math.pow(1 - e2 * sin2Lat, 3. / 2.);
 		double rho = num / denom;
 		return rho;
 	}
-
+	
 	private double calcV(double sin2Lat) {
 		double poly = 1 - e2 * sin2Lat;
 		double v = a / Math.sqrt(poly);
 		return v;
 	}
-
+	
+	/**
+	 * Computes Latitude and Longitude based on an SVY21 coordinate.
+	 * 
+	 * This method returns a Java Map containing exactly two entries. 
+	 * Latitude can be accessed by the key returned by the static method latitudeKey().
+	 * Longitude can be accessed by the key returned by the static method longitudeKey().
+	 * 
+	 * This method is a short-hand for the functionally-identical
+	 * public Map<String, Double> computeLatLon(double N, double E)
+	 * 
+	 * @param svy21Coordinate
+	 * @return
+	 */
+	public Map<String, Double> computeLatLon(Map<String, Double> svy21Coordinate) {
+		double northing = svy21Coordinate.get(northingKey());
+		double easting = svy21Coordinate.get(eastingKey());
+		return computeLatLon(northing, easting);
+	}
+	
+	/**
+	 * Computes SVY21 Northing and Easting based on a Latitude and Longitude coordinate.
+	 * 
+	 * This method returns a Java Map containing exactly two entries. 
+	 * Northings can be accessed by the key returned by the static method northingKey().
+	 * Eastings can be accessed by the key returned by the static method eastingKey().
+	 * 
+	 * This method is a short-hand for the functionally-identical
+	 * public Map<String, Double> computeSVY21(double lat, double lon)
+	 * 
+	 * @param latlonCoordinate
+	 * @return
+	 */
+	public Map<String, Double> computeSVY21(Map<String, Double> latlonCoordinate) {
+		double latitude = latlonCoordinate.get(latitudeKey());
+		double longitude = latlonCoordinate.get(longitudeKey());
+		return computeSVY21(latitude, longitude);
+	}
+	
+	/**
+	 * Computes Latitude and Longitude based on an SVY21 coordinate.
+	 * 
+	 * This method returns a Java Map containing exactly two entries. 
+	 * Latitude can be accessed by the key returned by the static method latitudeKey().
+	 * Longitude can be accessed by the key returned by the static method longitudeKey().
+	 * 
+	 * @param N Northing based on SVY21.
+	 * @param E Easting based on SVY21.
+	 * @return the conversion result as a Java Map.
+	 */
 	public Map<String, Double> computeLatLon(double N, double E) {
 		double Nprime = N - No;
 		double Mo = calcM(oLat);
@@ -170,19 +215,67 @@ public class SVY21 {
 		return result;
 	}
 	
-	public String latitudeKey() {
-		return KEY_LATITUDE;
-	}
-	
-	public String longitudeKey() {
-		return KEY_LONGITUDE;
-	}
-	
-	public String northingKey() {
-		return KEY_NORTHING;
-	}
-	
-	public String eastingKey() {
-		return KEY_EASTING;
+	/**
+	 * Computes SVY21 Northing and Easting based on a Latitude and Longitude coordinate.
+	 * 
+	 * This method returns a Java Map containing exactly two entries. 
+	 * Northings can be accessed by the key returned by the static method northingKey().
+	 * Eastings can be accessed by the key returned by the static method eastingKey().
+	 * 
+	 * @param lat latitude in degrees.
+	 * @param lon longitude in degrees.
+	 * @return the conversion result as a Java Map.
+	 */
+	public Map<String, Double> computeSVY21(double lat, double lon) {
+		double latR = lat * radRatio;
+		double sinLat = Math.sin(latR);
+		double sin2Lat = sinLat * sinLat;
+		double cosLat = Math.cos(latR);
+		double cos2Lat = cosLat * cosLat;
+		double cos3Lat = cos2Lat * cosLat;
+		double cos4Lat = cos3Lat * cosLat;
+		double cos5Lat = cos3Lat * cos2Lat;
+		double cos6Lat = cos5Lat * cosLat;
+		double cos7Lat = cos5Lat * cos2Lat;
+
+		double rho = calcRho(sin2Lat);
+		double v = calcV(sin2Lat);
+		double psi = v / rho;
+		double t = Math.tan(latR);
+		double w = (lon - oLon) * radRatio;
+
+		double M = calcM(lat);
+		double Mo = calcM(oLat);
+
+		double w2 = w * w;
+		double w4 = w2 * w2;
+		double w6 = w4 * w2;
+		double w8 = w6 * w2;
+
+		double psi2 = psi * psi;
+		double psi3 = psi2 * psi;
+		double psi4 = psi2 * psi2;
+
+		double t2 = t * t;
+		double t4 = t2 * t2;
+		double t6 = t4 * t2;
+
+		// Compute Northing.
+		double nTerm1 = w2 / 2 * v * sinLat * cosLat;
+		double nTerm2 = w4 / 24 * v * sinLat * cos3Lat * (4 * psi2 + psi - t2);
+		double nTerm3 = w6 / 720 * v * sinLat * cos5Lat * ((8 * psi4) * (11 - 24 * t2) - (28 * psi3) * (1 - 6 * t2) + psi2 * (1 - 32 * t2) - psi * 2 * t2 + t4);
+		double nTerm4 = w8 / 40320 * v * sinLat * cos7Lat * (1385 - 3111 * t2 + 543 * t4 - t6);
+		double N = No + k * (M - Mo + nTerm1 + nTerm2 + nTerm3 + nTerm4);
+
+		// Compute Easting.
+		double eTerm1 = w2 / 6 * cos2Lat * (psi - t2);
+		double eTerm2 = w4 / 120 * cos4Lat * ((4 * psi3) * (1 - 6 * t2) + psi2 * (1 + 8 * t2) - psi * 2 * t2 + t4);
+		double eTerm3 = w6 / 5040 * cos6Lat * (61 - 479 * t2 + 179 * t4 - t6);
+		double E = Eo + k * v * w * cosLat * (1 + eTerm1 + eTerm2 + eTerm3);
+
+		Map<String, Double> result = new TreeMap<String, Double>();
+		result.put(northingKey(), N);
+		result.put(eastingKey(), E);
+		return result;
 	}
 }
