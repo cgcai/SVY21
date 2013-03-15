19da585baf4d97ae20c3b9124b9437e6ec53eee7
Jerome Cheng <jcheng31@gmail.com>
2013-03-15 19:59:03 +0800

    Add equality methods to LatLongCoordinate and Svy21Coordinate.

--- csharp/svy21/LatLongCoordinate.cs
+++ csharp/svy21/LatLongCoordinate.cs
@@ -8,6 +8,19 @@ namespace SVY21
 {
     public class LatLongCoordinate
     {
+        protected bool Equals(LatLongCoordinate other)
+        {
+            return Latitude.Equals(other.Latitude) && Longitude.Equals(other.Longitude);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Latitude.GetHashCode()*397) ^ Longitude.GetHashCode();
+            }
+        }
+
         public double Latitude { get; set; }
         public double Longitude { get; set; }
 
@@ -20,6 +33,14 @@ namespace SVY21
         public Svy21Coordinate ToSvy21Coordinate()
         {
             return Svy21.ComputeSvy21(this);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((LatLongCoordinate) obj);
         }
     }
 }
--- csharp/svy21/Svy21Coordinate.cs
+++ csharp/svy21/Svy21Coordinate.cs
@@ -8,6 +8,14 @@ namespace SVY21
 {
     public class Svy21Coordinate
     {
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Easting.GetHashCode()*397) ^ Northing.GetHashCode();
+            }
+        }
+
         public double Easting { get; set; }
         public double Northing { get; set; }
 
@@ -20,6 +28,19 @@ namespace SVY21
         public LatLongCoordinate ToLatLongCoordinate()
         {
             return Svy21.ComputeLatitudeLongitude(this);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Svy21Coordinate) obj);
+        }
+
+        protected bool Equals(Svy21Coordinate other)
+        {
+            return Easting.Equals(other.Easting) && Northing.Equals(other.Northing);
         }
     }
 }
