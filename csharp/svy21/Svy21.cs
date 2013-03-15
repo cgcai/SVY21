19460c56552d6f2472753a28354d894aaf8953e3
Jerome Cheng <jcheng31@gmail.com>
2013-03-15 19:44:54 +0800

    Add method skeletons to Svy21.

--- csharp/svy21/Svy21.cs
+++ csharp/svy21/Svy21.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,37 @@ namespace SVY21
 {
     public class Svy21
     {
-        public static Svy21Coordinate ComputeSvy21(LatLongCoordinate latLongCoordinate)
+        private static double CalculateMeridian(double latitude)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static double CalculateRho(double sinSquaredLatitude)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static double CalculateCurvature(double sinSquaredLatitude)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static LatLongCoordinate ComputeLatitudeLongitude(double northing, double easting)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static LatLongCoordinate ComputeLatitudeLongitude(Svy21Coordinate coordinate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Svy21Coordinate ComputeSvy21(double latitude, double longitude)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Svy21Coordinate ComputeSvy21(LatLongCoordinate coordinate)
         {
             throw new NotImplementedException();
         }
