fd0ee0b50002ef44945c773b5722bd8522450399
Jerome Cheng <jcheng31@gmail.com>
2013-03-15 19:48:55 +0800

    Add unit test file.

---
+++ csharp/SvyTests/Svy21Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SvyTests
+{
+    [TestClass]
+    public class Svy21Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
