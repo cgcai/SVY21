b82a7298e659cd62ca7beaf30f24984b8780c5ca
Jerome Cheng <jcheng31@gmail.com>
2013-03-15 19:29:48 +0800

    Add skeletons for classes.

---
+++ csharp/svy21/LatLongCoordinate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVY21
+{
+    public class LatLongCoordinate
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public LatLongCoordinate(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public Svy21Coordinate ToSvy21()
+        {
+            return Svy21.ComputeSvy21(this);
+        }
+    }
+}
---
+++ csharp/svy21/Svy21.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVY21
+{
+    public class Svy21
+    {
+        public static Svy21Coordinate ComputeSvy21(LatLongCoordinate latLongCoordinate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
