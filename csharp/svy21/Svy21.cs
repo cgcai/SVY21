e496f39af5bf8f1c396c6c3cb107efd6b5a4048c
Jerome Cheng <jcheng31@gmail.com>
2013-03-16 10:50:29 +0800

    Remove unused usings.

--- csharp/svy21/Svy21.cs
+++ csharp/svy21/Svy21.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SVY21
 {
--- csharp/svy21/Svy21Coordinate.cs
+++ csharp/svy21/Svy21Coordinate.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SVY21
+﻿namespace SVY21
 {
     public class Svy21Coordinate
     {
