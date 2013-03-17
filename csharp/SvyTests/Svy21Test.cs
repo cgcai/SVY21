7885189d3b206ff3ff2cc01a0b80fa9388b59e6d
Jerome Cheng <jcheng31@gmail.com>
2013-03-17 22:25:20 +0800

    Add tolerance to SVY21 conversion tests.

--- csharp/SvyTests/Svy21Test.cs
+++ csharp/SvyTests/Svy21Test.cs
@@ -6,7 +6,8 @@ namespace SvyTests
     [TestClass]
     public class Svy21Test
     {
-        private const double Tolerance = 0.0000000001;
+        private const double ToleranceLatLong = 0.0000000001;
+        private const double ToleranceSvy21 = 0.001;
 
         [TestMethod]
         public void Svy21ToLatLongTest()
@@ -18,29 +19,29 @@ namespace SvyTests
             expected = new LatLongCoordinate(1.366666, 103.833333);
             test = new Svy21Coordinate(38744.572, 28001.642);
             actual = Svy21.ComputeLatitudeLongitude(test);
-            Assert.AreEqual(expected.Latitude, actual.Latitude, Tolerance);
-            Assert.AreEqual(expected.Longitude, actual.Longitude, Tolerance);
+            Assert.AreEqual(expected.Latitude, actual.Latitude, ToleranceLatLong);
+            Assert.AreEqual(expected.Longitude, actual.Longitude, ToleranceLatLong);
 
             // Corner of Ang Mo Kio 66kV Substation.
             expected = new LatLongCoordinate(1.3699278977737488, 103.856950349764668);
             test = new Svy21Coordinate(39105.269, 30629.967);
             actual = Svy21.ComputeLatitudeLongitude(test);
-            Assert.AreEqual(expected.Latitude, actual.Latitude, Tolerance);
-            Assert.AreEqual(expected.Longitude, actual.Longitude, Tolerance);
+            Assert.AreEqual(expected.Latitude, actual.Latitude, ToleranceLatLong);
+            Assert.AreEqual(expected.Longitude, actual.Longitude, ToleranceLatLong);
             
             // Corner of Jurong Lake Canal 400kV Cable Bridge.
             expected = new LatLongCoordinate(1.3446255443241177, 103.72794378041792);
             test = new Svy21Coordinate(36307.704, 16272.970);
             actual = Svy21.ComputeLatitudeLongitude(test);
-            Assert.AreEqual(expected.Latitude, actual.Latitude, Tolerance);
-            Assert.AreEqual(expected.Longitude, actual.Longitude, Tolerance);
+            Assert.AreEqual(expected.Latitude, actual.Latitude, ToleranceLatLong);
+            Assert.AreEqual(expected.Longitude, actual.Longitude, ToleranceLatLong);
 
             // Corner of Sembawang 66kV Substation.
             expected = new LatLongCoordinate(1.4520670518379692, 103.83080332777138);
             test = new Svy21Coordinate(48187.789, 27720.130);
             actual = Svy21.ComputeLatitudeLongitude(test);
-            Assert.AreEqual(expected.Latitude, actual.Latitude, Tolerance);
-            Assert.AreEqual(expected.Longitude, actual.Longitude, Tolerance);
+            Assert.AreEqual(expected.Latitude, actual.Latitude, ToleranceLatLong);
+            Assert.AreEqual(expected.Longitude, actual.Longitude, ToleranceLatLong);
         }
 
         [TestMethod]
@@ -53,25 +54,29 @@ namespace SvyTests
             expected = new Svy21Coordinate(38744.572, 28001.642);
             test = new LatLongCoordinate(1.366666, 103.833333);
             actual = Svy21.ComputeSvy21(test);
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected.Northing, actual.Northing, ToleranceSvy21);
+            Assert.AreEqual(expected.Easting, actual.Easting, ToleranceSvy21);
 
             // Corner of Ang Mo Kio 66kV Substation.
             expected = new Svy21Coordinate(39105.269, 30629.967);
             test = new LatLongCoordinate(1.3699278977737488, 103.85695034976466);
             actual = Svy21.ComputeSvy21(test);
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected.Northing, actual.Northing, ToleranceSvy21);
+            Assert.AreEqual(expected.Easting, actual.Easting, ToleranceSvy21);
 
             // Corner of Jurong Lake Canal 400kV Cable Bridge.
             expected = new Svy21Coordinate(36307.704, 16272.970);
             test = new LatLongCoordinate(1.3446255443241177, 103.72794378041792);
             actual = Svy21.ComputeSvy21(test);
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected.Northing, actual.Northing, ToleranceSvy21);
+            Assert.AreEqual(expected.Easting, actual.Easting, ToleranceSvy21);
 
             // Corner of Sembawang 66kV Substation.
             expected = new Svy21Coordinate(48187.789, 27720.130);
             test = new LatLongCoordinate(1.4520670518379692, 103.83080332777138);
             actual = Svy21.ComputeSvy21(test);
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected.Northing, actual.Northing, ToleranceSvy21);
+            Assert.AreEqual(expected.Easting, actual.Easting, ToleranceSvy21);
         }
     }
 }
